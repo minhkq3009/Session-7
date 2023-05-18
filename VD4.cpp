dce2612767544f849351385f25c6bff72238f94a
minhkq3009 <55178535+minhkq3009@users.noreply.github.com>
2023-05-18 08:41:04 +0700

    Add files via upload

---
+++ VD4.cpp
@@ -0,0 +1,10 @@
+#include <stdio.h>
+main(void){
+	char c;
+	printf("Please enter a character:");
+	scanf("%c", &c);
+	if (c>='A' && c <='Z')
+		printf ("Lowercase character = %c", c+'a'-'A');
+	else
+		printf("Character Entered is = %c",c);
+	}
