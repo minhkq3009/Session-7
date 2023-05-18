6c9e18f6885b8daee3fb5e8926f0ccc4ebc8c5c7
minhkq3009 <55178535+minhkq3009@users.noreply.github.com>
2023-05-18 08:29:31 +0700

    Add files via upload

---
+++ VD3.cpp
@@ -0,0 +1,11 @@
+#include <stdio.h>
+main(void) {
+	int num, res;
+	printf("Enter a number:");
+	scanf("%d",&num);
+	res = num % 2;
+	if (res == 0)
+		printf("The number is Even");
+	else
+		printf("The number is Odd");
+	}
